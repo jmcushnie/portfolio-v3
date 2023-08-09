6e350da83f618563f03529f1f2f102d2860fd590
Jess Cushnie <94955791+jmcushnie@users.noreply.github.com>
2023-08-10 11:46:59 +1200

    initial commit
    Nav bar

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section className="hero-section">
+      <h1> CREATIVE DESIGNER & DEVELOPER</h1>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/navBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav>
+      <ul className="nav-links">
+        <li>
+          <a href="#">RESUME</a>
+        </li>
+        <li>
+          <a href="#">PROJECTS</a>
+        </li>
+        <li>
+          <a href="#">CONTACT</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
